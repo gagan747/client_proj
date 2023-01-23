a620228d7665852cf223f0337ce8486c58301ab2
gurjot74 <gagan.singh@tothenew.com>
2023-01-24 04:44:10 +0530

    added video

--- client/src/Components/Box.jsx
+++ client/src/Components/Box.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import xyz from './xyz.mp4';
 
 export default function Box({ bgUrl, btnMsg, distanceFromBottom }) {
   return (
-    <div className='box' style={{backgroundImage:`url(${bgUrl})`}}>
-      <span className='box-button' style={{ bottom:`${distanceFromBottom}px`}}>{btnMsg}</span>
+    <div className="box">
+<video id="box" src={xyz} type="video/mp4" autoPlay loop muted />
+ <span className='box-button' style={{ bottom:`${distanceFromBottom}px`}}>{btnMsg}</span>
     </div>
   )
 }
