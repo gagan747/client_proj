b49a3fc7915856fd081eca1b6aa4b926eca29a51
gurjot74 <gagan.singh@tothenew.com>
2023-02-07 04:31:32 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import Boxes from "./Components/Boxes";
+import 'react-toastify/dist/ReactToastify.css';
 import { useSelector } from "react-redux"
 import Navbar from './Components/Navbar'
 import Searchbar from "./Components/Searchbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Products from "./Components/Products";
+import Login from './Components/Login'
+import Signup from './Components/Signup'
 
 function App() {
   const state = useSelector((state) => state.Header);
@@ -13,10 +16,10 @@ function App() {
       {state.Navbar ? <Navbar /> : <Searchbar /> }
       <BrowserRouter>
         <Routes> 
-          <Route path="/" element={<><Boxes /></>}>
-          </Route>
-          <Route path="/collection" element={<><Products /></>}>
-          </Route>
+          <Route path="/" element={<><Boxes /></>} />
+          <Route path="/signup" element={<><Signup /></>} />
+          <Route path="/login" element={<><Login /></>} />
+          <Route path="/collection" element={<><Products /></>} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/Components/Login.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import messageCleaner from '../utils/messageCleaner'
+import { hostUrl } from '../config'
+
+export default function Login() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+  const navigate = useNavigate();
+  const login = async () => {
+    try {
+      const res = await fetch(
+        `${hostUrl}/api/auth/login`,
+        {
+          method: 'POST',
+          body: JSON.stringify(formData),
+          headers: {
+            'content-type': 'application/json',
+          },
+        },
+      );
+      const data = await res.json();
+      if (res.status === 200) {
+        localStorage.setItem('x-auth-token', res.headers.get('x-auth-token'));
+        localStorage.setItem('username', data.username);
+        toast.success(data.message, {
+          toastId: 'login',
+        });
+        navigate('/')
+      }
+      else {
+        toast.error(messageCleaner(data.message), {
+          toastId: 'login',
+        });
+
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error(messageCleaner(err), { toastId: 'loginerror' })
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login()
+  };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <form className='login'>
+      <h1>
+        Login
+      </h1>
+<input type='email' placeholder='Username...' />
+<input type='password' placeholder='Password...' />
+    <button type='submit'>Login</button>
+    <div>
+      <Link to='/signup'>Sign Up?</Link>
+        <Link to='/forgotpassword'>Forgot Password?</Link>
+    </div>
+     
+
+    </form>
+  )
+}
